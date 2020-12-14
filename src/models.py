f812fcd3f923ef7e89737ba948337fb67212820b
SSinyu <shsy0404g@gmail.com>
2020-12-14 14:48:08 +0900

    Add UNet models

--- src/models.py
+++ src/models.py
@@ -1,6 +1,11 @@
 
 def get_model(model_type):
-    if model_type == "deeplabv3p":
+    if model_type.lower() == "deeplabv3p":
         from src.deeplab.networks import DeepLabV3pXc
         return DeepLabV3pXc
-        
+    elif model_type.lower() == "unet":
+        from src.unet.networks import UNet
+        return UNet
+    elif model_type.lower() in ["attentionunet", "attunet"]:
+        from src.unet.networks import AttentionUNet
+        return AttentionUNet
